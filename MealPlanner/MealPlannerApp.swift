b54b9b3d10132e8f657a7feefffbc457b2cf2935
LST <lucianstan88@gmail.com>
2026-02-10 10:03:20 +0000

    Initial Commit

---
+++ MealPlanner/MealPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealPlannerApp.swift
+//  MealPlanner
+//
+//  Created by Lucian Stan on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MealPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
